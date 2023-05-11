d8761c546f576e224acfb86a4b808e871bbc3978
Jakub Rychlicki <crespor00r@gmail.com>
2023-05-11 22:18:57 +0200

    add fonts

---
+++ src/constants/Fonts.ts
@@ -0,0 +1,7 @@
+export default {
+  NunitoRegular: 'Nunito-Regular',
+  NunitoMedium: 'Nunito-Medium',
+  NunitoSemiBold: 'Nunito-SemiBold',
+  RobotoRegular: 'Roboto-Regular',
+  RobotoMedium: 'Roboto-Medium',
+};
