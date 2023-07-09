9355361545b5c746f749957be641fe23295d64b9
Jakub Rychlicki <crespor00r@gmail.com>
2023-07-09 21:35:35 +0200

    feat: add mapbox

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import { I18nextProvider } from 'react-i18next';
 import MainNavigator from './src/navigation/MainNavigator';
 import i18n from './src/assets/translations';
+import { MAPBOX_TOKEN } from '@env';
+import Mapbox from '@rnmapbox/maps';
+
+Mapbox.setAccessToken(MAPBOX_TOKEN);
 
 function App() {
   return (
--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Dimensions } from 'react-native';
 import { MainNavigatorScreen } from '../../navigation/MainNavigator';
 import { useAppDispatch, useAppSelector } from '../../hooks/useAppDispatch';
 import * as actions from '../../store/actions';
+import Mapbox from '@rnmapbox/maps';
+
+const { width } = Dimensions.get('window');
 
 const HomeScreen: MainNavigatorScreen<'HomeScreen'> = ({ navigation, route }) => {
   const dispatch = useAppDispatch();
@@ -15,6 +18,9 @@ const HomeScreen: MainNavigatorScreen<'HomeScreen'> = ({ navigation, route }) =>
   return (
     <View style={styles.container}>
       <Text>HomeScreen</Text>
+      <View style={styles.mapContainer}>
+        <Mapbox.MapView style={styles.map} />
+      </View>
       <Button title="sign out" onPress={signout} />
     </View>
   );
@@ -24,6 +30,13 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  mapContainer: {
+    width: width,
+    height: 300,
+  },
+  map: {
     flex: 1,
   },
 });
